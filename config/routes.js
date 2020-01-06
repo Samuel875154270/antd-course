c926216fbd9cba79b3eac43644651b69f0dcee34
sam.lee <sam.lee@songmao.tech>
2020-01-06 18:02:46 +0800

    布局与路由

--- config/routes.js
+++ config/routes.js
@@ -1,16 +1,32 @@
-let routes = [
-    {
-        path: '/',
-        component: './HelloWorld',
-    },
-    {
-        path: '/c',
-        component: './content',
-    },
-    {
-        path: '/my',
-        component: './MyComponent',
-    }
-];
+let routes = [{
+    path: '/',
+    component: '../layout',
+    routes: [
+        {
+            path: '/',
+            component: './HelloWorld',
+        },
+        {
+            path: 'helloworld',
+            component: './HelloWorld',
+        },
+        {
+            path: '/dashboard',
+            routes: [
+                { path: '/dashboard/analysis', component: 'Dashboard/Analysis' },
+                { path: '/dashboard/monitor', component: 'Dashboard/Monitor' },
+                { path: '/dashboard/workplace', component: 'Dashboard/Workplace' }
+            ]
+        },
+        {
+            path: 'content',
+            component: './content',
+        },
+        {
+            path: '/mycomponent',
+            component: './MyComponent',
+        }
+    ]
+}];
 
 export default routes;
