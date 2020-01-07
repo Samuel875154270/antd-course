96d50ba82e45b4d7120271ae04329426d52bea46
sam.lee <sam.lee@songmao.tech>
2020-01-07 14:44:52 +0800

    使用dva的connect对接静态的 dva model；dispatch 和 reducer改变 dva model

--- src/model/README.md
+++ src/model/README.md
@@ -1,1 +1,34 @@
 负责处理业务逻辑，为 Page 做数据、状态的读写、变换、暂存等。
+
+一个简单的 model 示例如下：https://www.yuque.com/ant-design/course/abl3ad
+```
+app.model({
+    
+    namespace: 'todoList',
+    
+    state: {},
+    
+    effects: { // 定义 effects 成员
+        *query({ _ }, { put, call }) {
+            const rsp = yield call(queryTodoListFromServer);
+            const todoList = rsp.data;
+            yield put({ type: 'save', payload: todoList });
+        },
+    },
+    
+    reducers: {
+        save(state, { payload: todoList }) {
+            return [...state, todoList];
+        },
+    },
+
+});
+```
+
+dva 中一个典型的 effect 的写法是：
+```
+getData: function* ({ payload }, { call, put }) {
+    const data = yield call(SomeService.getEndpointData, payload, 'maybeSomeOtherParams');
+    yield put({ type: 'getData_success', payload: data });
+}
+```
--- src/page/puzzlecards.js
+++ src/page/puzzlecards.js
@@ -1,61 +1,113 @@
 import React, { Component } from 'react';
-import { Card, Button } from 'antd';
+import { Card ,Button } from 'antd';
+import { connect } from 'dva';
 
-export default class PuzzleCardsPage extends Component {
-    constructor(props) {
-        super(props);
+const namespace = 'puzzlecards';
 
-        this.counter = 100;
-        this.state = {
-            cardList: [
-                {
-                    id: 1,
-                    setup: 'Did you hear about the two silk worms in a race?',
-                    punchline: 'It ended in a tie',
-                },
-                {
-                    id: 2,
-                    setup: 'What happens to a frog\'s car when it breaks down?',
-                    punchline: 'It gets toad away',
-                },
-            ],
-        }
-    }
-
-    addNewCard = () => {
-        this.setState(prevState => {
-            const prevCardList = prevState.cardList;
-            this.counter += 1;
-            const card = {
-                id: this.counter,
-                setup: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
-                punchline: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-            };
-            return {
-                cardList: prevCardList.concat(card),
-            };
-        });
+const mapStateToProps = (state) => {
+    const cardList = state[namespace].data;
+    return {
+        cardList,
     };
+};
 
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onClickAdd: (newCard) => {
+            const action = {
+                type: `${namespace}/addNewCard`,
+                payload: newCard,
+            };
+            dispatch(action);
+        },
+    };
+};
+
+// dva写法
+@connect(mapStateToProps, mapDispatchToProps)
+export default class PuzzleCardsPage extends Component {
     render() {
         return (
             <div>
                 {
-                    this.state.cardList.map(card => {
-                            return (
-                                <Card key={card.id}>
-                                    <div>ID: {card.id}</div>
-                                    <div>Q: {card.setup}</div>
-                                    <div><strong>A: {card.punchline}</strong></div>
-                                </Card>
-                            );
-                        }
-                    )
+                    this.props.cardList.map(card => {
+                        return (
+                            <Card key={card.id}>
+                                <div>Q: {card.setup}</div>
+                                <div>
+                                    <strong>A: {card.punchline}</strong>
+                                </div>
+                            </Card>
+                        );
+                    })
                 }
                 <div>
-                    <Button onClick={this.addNewCard}>添加卡片</Button>
+                    <Button onClick={() => this.props.onClickAdd({
+                        setup: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                        punchline: 'here we use dva',
+                    })}>添加卡片</Button>
                 </div>
             </div>
         );
     }
 }
+
+// 没有dva写法
+// export default class PuzzleCardsPage extends Component {
+//     constructor(props) {
+//         super(props);
+//
+//         this.counter = 100;
+//         this.state = {
+//             cardList: [
+//                 {
+//                     id: 1,
+//                     setup: 'Did you hear about the two silk worms in a race?',
+//                     punchline: 'It ended in a tie',
+//                 },
+//                 {
+//                     id: 2,
+//                     setup: 'What happens to a frog\'s car when it breaks down?',
+//                     punchline: 'It gets toad away',
+//                 },
+//             ],
+//         }
+//     }
+//
+//     addNewCard = () => {
+//         this.setState(prevState => {
+//             const prevCardList = prevState.cardList;
+//             this.counter += 1;
+//             const card = {
+//                 id: this.counter,
+//                 setup: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
+//                 punchline: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+//             };
+//             return {
+//                 cardList: prevCardList.concat(card),
+//             };
+//         });
+//     };
+//
+//     render() {
+//         return (
+//             <div>
+//                 {
+//                     this.state.cardList.map(card => {
+//                             return (
+//                                 <Card key={card.id}>
+//                                     <div>ID: {card.id}</div>
+//                                     <div>Q: {card.setup}</div>
+//                                     <div><strong>A: {card.punchline}</strong></div>
+//                                 </Card>
+//                             );
+//                         }
+//                     )
+//                 }
+//                 <div>
+//                     <Button onClick={this.addNewCard}>添加卡片</Button>
+//                 </div>
+//             </div>
+//         );
+//     }
+// }
