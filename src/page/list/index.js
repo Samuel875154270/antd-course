c95770fff715f4f18a946fddb4e5d44abb88b294
Sam <sam.li@doohui.com>
2020-01-29 18:00:27 +0800

    图表

--- src/page/list/index.js
+++ src/page/list/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import { Table } from 'antd';
+import { Table, Modal, Button, Form, Input } from 'antd';
 import { connect } from 'dva';
+import SampleChart from "../../component/SampleChart";
+
+const FormItem = Form.Item;
 
 class List extends React.Component {
+    state = {
+        visible: false,
+        statisticVisible: false,
+        id: null,
+    };
+
     columns = [
         {
             title: '名称',
@@ -15,9 +24,60 @@ class List extends React.Component {
         {
             title: '链接',
             dataIndex: 'url',
-            render: value => <a href={value}>{value}</a>,
+            render: value => <a href={ value }>{ value }</a>,
+        },
+        {
+            title: '',
+            dataIndex: '_',
+            render: (_, { id }) => {
+                return (
+                    <Button onClick={ () => {
+                        this.showStatistic(id)
+                    } }>图表</Button>
+                );
+            },
         },
     ];
+
+    showModal = () => {
+        this.setState({ visible: true });
+    };
+
+    handleCancel = () => {
+        this.setState({
+            visible: false,
+        });
+    };
+
+    handleOk = () => {
+        const { dispatch, form: { validateFields } } = this.props;
+
+        validateFields((err, values) => {
+            if (!err) {
+                dispatch({
+                    type: 'cards/addOne',
+                    payload: values,
+                });
+                // 重置 `visible` 属性为 false 以关闭对话框
+                this.setState({ visible: false });
+            }
+        });
+    };
+
+    showStatistic = (id) => {
+        this.props.dispatch({
+            type: 'cards/getStatistic',
+            payload: id,
+        });
+        // 更新 state，弹出包含图表的对话框
+        this.setState({ id, statisticVisible: true });
+    };
+
+    handleStatisticCancel = () => {
+        this.setState({
+            statisticVisible: false,
+        });
+    };
 
     componentDidMount() {
         this.props.dispatch({
@@ -26,10 +86,45 @@ class List extends React.Component {
     }
 
     render() {
-        const { cardsList, cardsLoading } = this.props;
+        const is_required = false;
+        const { visible, statisticVisible, id } = this.state;
+        const { cardsList, cardsLoading, form: { getFieldDecorator }, statistic } = this.props;
+
         return (
             <div>
-                <Table columns={this.columns} dataSource={cardsList} loading={cardsLoading} rowKey="id"/>
+                <Table columns={ this.columns } dataSource={ cardsList } loading={ cardsLoading } rowKey="id"/>
+                <Button onClick={ this.showModal }>新建</Button>
+                <Modal
+                    title="新建记录"
+                    visible={ visible }
+                    onOk={ this.handleOk }
+                    onCancel={ this.handleCancel }
+                >
+                    <Form>
+                        <FormItem label="名称">
+                            { getFieldDecorator('name', {
+                                rules: [{ required: is_required }],
+                            })(
+                                <Input/>
+                            ) }
+                        </FormItem>
+                        <FormItem label="描述">
+                            { getFieldDecorator('desc')(
+                                <Input/>
+                            ) }
+                        </FormItem>
+                        <FormItem label="链接">
+                            { getFieldDecorator('url', {
+                                rules: [{ type: 'url' }],
+                            })(
+                                <Input/>
+                            ) }
+                        </FormItem>
+                    </Form>
+                </Modal>
+                <Modal visible={ statisticVisible } footer={ null } onCancel={ this.handleStatisticCancel }>
+                    <SampleChart data={ statistic[id] }/>
+                </Modal>
             </div>
         );
     }
@@ -39,7 +134,8 @@ function mapStateToProps(state) {
     return {
         cardsList: state.cards.cardsList,
         cardsLoading: state.loading.effects['cards/queryList'],
+        statistic: state.cards.statistic,
     };
 }
 
-export default connect(mapStateToProps)(List);
+export default connect(mapStateToProps)(Form.create()(List));
